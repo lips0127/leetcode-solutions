b7c6e96e3955d72ed1f83bc2b0321ef2e15cf077
weizhou <869066874@qq.com>
2025-07-06 01:44:12 +0800

    fix:generateTests获取不到继承Solution子类的问题.

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -1,9 +1,11 @@
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 import org.weizhou.solutions.core.Solution;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -13,8 +15,11 @@ public class RunnerTest {
     @TestFactory
     public List<DynamicTest> generateTests() throws Exception {
         List<DynamicTest> tests = new ArrayList<DynamicTest>();
-        Reflections reflections = new Reflections("solutions");
-
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .setUrls(ClasspathHelper.forPackage("org.weizhou.solutions"))
+                .setScanners(Scanners.SubTypes)
+                .filterInputsBy(s -> s.endsWith(".class"))
+        );
         // 自动发现所有Solution实现类
         Set<Class<? extends Solution>> subTypesOfSolutions = reflections.getSubTypesOf(Solution.class);
         for (Class<?> clazz : subTypesOfSolutions) {
